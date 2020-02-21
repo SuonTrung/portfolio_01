39d18d75eb5505889dd6cb2706c40dd0fea28647
SuonTrung <61301769+SuonTrung@users.noreply.github.com>
2020-02-21 17:25:36 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+$(".btn.sp").click(function() {
+  $("#top").toggleClass("s_on");
+  $("#mid").toggleClass("s_on");
+  $("#end").toggleClass("s_on");
+  $(".humb").toggleClass("s_on");
+  $("body").toggleClass("hidden");
+})
